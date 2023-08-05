e93150006e01f0c4b8c56fe24ec9c0bedd4b1cf2
Cristhian Castro <84578335+Cristhian9507@users.noreply.github.com>
2023-08-05 17:30:42 -0500

    [OC-1] se crean las migraciones necesarias

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('identificacion', 11);
+            $table->string('nombre', 100);
+            $table->integer('celular')->nullable();
+            $table->date('fecha_nacimiento');
+            $table->integer('ciudad_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes()->nullable();
             $table->timestamps();
         });
     }
